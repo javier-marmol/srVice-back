a5e1624aa67067db0d195d887c07d314f6f6939d
javier-marmol <javier.marmolaramendia@gmail.com>
2022-05-22 21:48:38 +0200

    job

--- src/main/java/com/javier/srvice/presentedTo/infrastructure/controller/dto/output/SimplePresentedToOutputDto.java
+++ src/main/java/com/javier/srvice/presentedTo/infrastructure/controller/dto/output/SimplePresentedToOutputDto.java
@@ -11,11 +11,13 @@ public class SimplePresentedToOutputDto {
     private Integer idEmployee;
     private String name;
     private Integer idJob;
+    private Integer idUser;
 
     public SimplePresentedToOutputDto(PresentedTo presentedTo){
         this.setId(presentedTo.getId());
         this.setIdEmployee(presentedTo.getEmployee().getId());
         this.setIdJob(presentedTo.getJob().getId());
         this.setName(presentedTo.getEmployee().getUser().getName());
+        this.setIdUser(presentedTo.getEmployee().getUser().getId());
     }
 }
